0ddb86d5143354937c8b5d2af6f4f1c7011b13ab
Lindsey Duquette <duquette.lr@gmail.com>
2019-06-24 21:41:27 -0400

    handlebars

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+var PORT = process.env.PORT || 1000;
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.get("/template", function (req, res) {
+    res.render("template");
+
+});
+
+app.get("/", function (req, res) {
+
+    res.render("index", {
+        //user chooses template they would like to use
+        //the chosen object is sent to this file and used to
+        //create a template on the home page
+        selectedTemplate
+    })
+});
+
+app.listen(PORT, function () {
+    console.log("listening");
+});

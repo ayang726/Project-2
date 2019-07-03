99e6214461f16458ad0be503d6b41e11c0a6a93d
Lindsey Duquette <duquette.lr@gmail.com>
2019-07-03 18:45:43 -0400

    deleted node modules

--- routes/apiRoutes-template.js
+++ routes/apiRoutes-template.js
@@ -1,23 +1,34 @@
+var mysql = require("mysql");
 const db = require("../models");
 
 //user selects metrics for template
 //post to Template User API/table and Template Metric API/table
 module.exports = function (app) {
-    app.post("/api/template", (req, res) => {
-        //how do I pass in the template id? it has not been created
-        db.TemplateMetric.create({
-            templateid: req.body.templateid,
-            metricid: req.body.metricid
-        }).then(response => {
-            console.log(response);
-        })
-
+    app.post("/api/template/", (req, res) => {
+        console.log(req.body);
         db.TemplateUser.create({
+            UserId: req.body.UserId,
+            templatename: req.body.templatename,
             uid: req.body.uid,
-            templatename: req.body.templatename
         }).then(response => {
             console.log(response);
-        })
-    });
 
-}
+            // connection.query("SELECT LAST_INSERT_ID()", function (err, result) {
+            //     if (err) {
+            //         throw err;
+            //     }
+            //how do I pass in the template id? it has not been created
+            var templateId = response.dataValues.id;
+            db.TemplateMetric.create({
+                templateid: templateId,
+                MetricId: req.body.MetricId
+            }).then(response => {
+                console.log(response);
+            })
+
+
+        });
+
+    });
+    // })
+};

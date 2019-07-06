a0c35b3106324b6078ace01f48f82ac5b553bf1e
Lindsey Duquette <duquette.lr@gmail.com>
2019-07-06 12:15:04 -0400

    changed primary key

--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User",
         {
             name: { type: DataTypes.STRING },
-            uid: { type: DataTypes.STRING },
+            // uid: { type: DataTypes.STRING },
+            id: { type: DataTypes.STRING, primaryKey: true },
             email: { type: DataTypes.STRING },
             password: { type: DataTypes.STRING }
         });

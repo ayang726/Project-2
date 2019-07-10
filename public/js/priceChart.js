14cd2adac79d5a0d5e12ca758180e8488c740328
Maria <lrabin@gmail.com>
2019-07-09 20:46:39 -0400

    priceChart.js = updated file with fix on priceChart hovering issue

--- public/js/priceChart.js
+++ public/js/priceChart.js
@@ -1,42 +1,43 @@
-function plotChart(dataSets, dataSetsLabel) {
-    ctx = document.getElementById('myChart').getContext('2d');
-    // TEST CODE:
-    //dataSets = [];
-    //dataSetsLabel = [];
-    myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: dataSetsLabel, //timeRange label
-            datasets: [{
-                label: 'Average Price',
-                data: dataSets, //prices
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
+let ctx = document.getElementById('myChart').getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: [], //timeRange label
+        datasets: [{
+            label: 'Average Price',
+            data: [], //prices
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
             }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
         }
-    });
+    }
+});
+
+function plotChart(dataSets, dataSetsLabel) {
+    myChart.data.labels = dataSetsLabel;
+    myChart.data.datasets[0].data = dataSets;
+    myChart.update();
 };

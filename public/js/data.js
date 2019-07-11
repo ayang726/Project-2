12248804e8a1ec0f6392f2ada9d61e33a5e4a11e
Maria <lrabin@gmail.com>
2019-07-10 20:57:30 -0400

    added logic to clear the re-graphing

--- public/js/data.js
+++ public/js/data.js
@@ -110,6 +110,7 @@ function updatingChart(period) {
     $.get("/api/chart/" + period + "/" + ticker, response => {
         var dataSetsLabel = [];
         var dataSets = [];
+        plotChart(dataSets, dataSetsLabel); //to clear the graph
         for (var i = 0; i < response.length; i++) {
             const dataObj = response[i];
             var volData = dataObj.close;
